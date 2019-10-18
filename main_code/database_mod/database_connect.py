482575b1697804eb358aee5e088401a810814f21
Hexagram <1471685806@qq.com>
2019-10-18 22:03:42 +0800

    fine 下一步 ：自动添加输入，方便输入

---
+++ main_code/database_mod/database_connect.py
@@ -0,0 +1,69 @@
+import pymysql
+import datetime
+
+
+state_machine = {
+    1:'spare',
+    2:'insert',
+    3:'change',
+    4:'delete',
+    5:'end',
+    6:'search',
+}
+
+class Database_control(object):
+    def __init__(self):
+        self.db = pymysql.connect("121.199.40.243","YuanCheng_user","_Hexagram_user","test_python")
+        self.cursor = self.db.cursor()
+    def print_data(self):
+            sql = "select * from events_arrangement"
+        
+            try:
+                self.cursor.execute(sql)
+                results = self.cursor.fetchall()
+                for row in results:
+                    _id = row[0]
+                    _title = row[1]
+                    _create_date = row[2]
+                    _end_time = row[3]
+                    _advance_warning_time = row[4]
+                    _extre_sth = row[5]
+
+                    print("%s,%s,%s,%s,%s,%s" % \
+                        (_id,_title,_create_date, \
+                        _end_time,_advance_warning_time,_extre_sth))
+            except:
+                print("error in fetch data")
+
+    def insert_into_database(self):
+        title = input("please input sth")
+        end_time = input("please input end time,\"- -\"")
+        if end_time == '':
+            end_time = '1111-11-11 11:11:11'
+
+        advance_warning_time = input("please input advance warning tima,\": :\"")
+        if advance_warning_time == '':
+            advance_warning_time = '11:11:11'
+            
+        extre_sth = input("please inpot extra sth")
+        create_time = datetime.datetime.now()
+        create_time_str = datetime.datetime.strftime(create_time,'%Y-%m-%d %H:%M:%S')
+
+        try:
+            sql = """
+                insert into events_arrangement 
+                (events_title,events_create_date,events_end_time,
+                events_advance_waring_time,extre_sth)
+                values
+                ('%s','%s','%s','%s','%s')
+                """ % (title,create_time_str,end_time,advance_warning_time,extre_sth)
+            #print(sql)
+        
+            self.cursor.execute(sql)
+            self.db.commit()
+        except:
+            print("wrong!!!")
+            self.db.rollback()
+        return state_machine[1]
+    def end_connect(self):
+        self.db.close()
--- main_code/first.py
+++ main_code/first.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
 import sys
-import pymysql
-import datetime
+import database_mod.database_connect
 
 state_machine = {
     1:'spare',
     2:'insert',
-    3:'',
+    3:'change',
     4:'delete',
     5:'end',
     6:'search',
 }
 class Main_control_machine(object):
     def __init__ (self):
-        self.database_control = Database_control()
+        self.database_control = database_mod.database_connect.Database_control()
         self.state = state_machine[1]
     def run(self):
         while self.state != 'end':
@@ -27,50 +26,11 @@ class Main_control_machine(object):
                 pass
             if self.state == 'search':
                 pass
+            if self.state == 'change':
+                pass
         else:
             self.database_control.end_connect()
             sys.exit(0)            
-
-class Database_control(object):
-    def __init__(self):
-        self.db = pymysql.connect("121.199.40.243","YuanCheng_user","_Hexagram_user","test_python")
-        self.cursor = self.db.cursor()
-    def print_data(self):
-            sql = "select * from events_arrangement"
-        
-            try:
-                self.cursor.execute(sql)
-                results = self.cursor.fetchall()
-                for row in results:
-                    pass
-            except:
-                print("error in fetch data")
-            print(results)
-
-    def insert_into_database(self):
-        title = input("please input sth")
-        end_time = input("please input end time,\"- -\"")
-        advance_warning_time = input("please input advance warning tima,\": :\"")
-        extre_sth = input("please inpot extra sth")
-        create_time = datetime.datetime.now()
-        create_time_str = datetime.datetime.strftime(create_time,'%Y-%m-%d %H:%M:%S')
-
-        sql = """
-            insert into events_arrangement 
-            (events_title,events_create_date,events_end_time,events_advance_waring_time,extre_sth)
-            values
-            ('%s','%s','%s','%s','%s')
-            """ % (title,create_time_str,end_time,advance_warning_time,extre_sth)
-        #print(sql)
-        #try:
-        self.cursor.execute(sql)
-        self.db.commit()
-        #except:
-        #    print("wrong!!!")
-        #    self.db.rollback()
-        return state_machine[1]
-    def end_connect(self):
-        self.db.close()
 
 def start():
     main_control_machine = Main_control_machine()
