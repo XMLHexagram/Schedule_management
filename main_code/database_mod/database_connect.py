b4fbc934cc7c88fdefec6343e2e1e3b62113ebdf
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2019-10-29 15:48:10 +0800

    fine

--- main_code/database_mod/database_connect.py
+++ main_code/database_mod/database_connect.py
@@ -20,25 +20,27 @@ state_machine = {
     4:'delete',
     5:'end',
     6:'search',
+    7:'change_to_daily',
+    8:'change_to_normal',
 }
 
 class Database_control(object):
     def __init__(self):
         pass
 
-    def print_data(self):
+    def print_data(self,temp_table):
         os.system('cls')
         print(state_machine)
         create_time = datetime.datetime.now()
         create_time = datetime.datetime.strftime(create_time,'%Y-%m-%d %H:%M:%S')
         print(create_time)
         print()
-        sql = "select * from events_arrangement"
-        self.__direct_database_control(sql)
+        sql = "select * from %s" % (temp_table)
+        self._direct_database_control(sql)
             
         # self.cursor.execute(sql)
         results = self.cursor.fetchall()
-        print(self.__direct_print(results))
+        print(self._direct_print(results))
 
     @log
     def change_database(self):
@@ -67,7 +69,7 @@ class Database_control(object):
             SET %s='%s'
             WHERE id=%d  
             """ % (colums[temp],temp_input,temp_id)
-        self.__direct_database_control(sql)
+        self._direct_database_control(sql)
         
         return state_machine[1]
 
@@ -76,11 +78,11 @@ class Database_control(object):
         title = input("please input sth:\n")
         end_time = input("please input end time,\"- -\":"  )
         if end_time == '':
-            end_time = '1111-11-11 11:11:11'
+            end_time = '1111-11-11 00:00:00'
 
         advance_warning_time = input("please input advance warning tima,\": :\":")
         if advance_warning_time == '':
-            advance_warning_time = '11:11:11'
+            advance_warning_time = '00:00:00'
             
         extre_sth = input("please inpot extra sth:\n")
         create_time = datetime.datetime.now()
@@ -94,7 +96,7 @@ class Database_control(object):
             ('%s','%s','%s','%s','%s')
             """ % (title,create_time_str,end_time,advance_warning_time,extre_sth)
             #print(sql)
-        self.__direct_database_control(sql)
+        self._direct_database_control(sql)
         return state_machine[1]
 
     @log
@@ -104,28 +106,28 @@ class Database_control(object):
             SELECT * FROM events_arrangement
             WHERE id = %d
             """ % (row_id)
-        self.__direct_database_control(sql)
+        self._direct_database_control(sql)
         results = self.cursor.fetchall()
-        self.__insert_into_file(results)
+        self._insert_into_file(results)
 
         sql = """
             DELETE FROM events_arrangement
             WHERE id = %d
             """ % (row_id)
-        self.__direct_database_control(sql)
+        self._direct_database_control(sql)
         return state_machine[1]
 
-    def __direct_database_control(self,sql):
-        self.__connect_database()
+    def _direct_database_control(self,sql):
+        self._connect_database()
         try:
             self.cursor.execute(sql)
             self.db.commit()
         except:
             print("wrong!!!")
             self.db.rollback()
-            time.sleep(5)
+            time.sleep(3)
 
-    def __direct_print(self,temp):
+    def _direct_print(self,temp):
         table = prettytable.PrettyTable(["ID","事务内容", \
             "创建时间","截至日期","提前预警时间","额外附加"])
         for row in temp:
@@ -141,18 +143,57 @@ class Database_control(object):
             )
         return table
 
-    def __insert_into_file(self,file_neirong):
+    def _insert_into_file(self,file_neirong):
         sth_to_say = input("please say sth about this events:\n")
         path1 = os.path.abspath('.')
         path1 = path1 + '/main_code/database_log/log.txt'
         # print(path1)
-        temp = self.__direct_print(file_neirong)
+        temp = self._direct_print(file_neirong)
         f = open(path1,mode='a',encoding='utf-8')
         f.write("\n\n%s\n%s" % (temp,sth_to_say))
         f.close
-    def __connect_database(self):
-        self.db = pymysql.connect("121.199.40.243","YuanCheng_user","_Hexagram_user","test_python")
-        self.cursor = self.db.cursor()
+
+    def _connect_database(self):
+        try:
+            self.db = pymysql.connect("121.199.40.243","YuanCheng_user","_Hexagram_user","test_python")
+            self.cursor = self.db.cursor()
+        except:
+            time.sleep(3)
+            self._connect_database()
 
     def end_connect(self):
         self.db.close()
+
+class daily_table_control(Database_control):
+    def _direct_print(self,temp):
+        table = prettytable.PrettyTable(["ID","事务内容", \
+            "推荐时间点","额外附加"])
+        for row in temp:
+            _id = row[0]
+            _title = row[1]
+            do_it_time = str(row[2])
+            _extre_sth = row[3]            
+            table.add_row( \
+            [_id,_title,do_it_time, \
+            _extre_sth] \
+            )
+        return table
+
+    @log
+    def insert_into_database(self):
+        title = input("please input sth:\n")
+        events_to_do_time = input("please input events_to_do_time,\": :\":"  )
+        if events_to_do_time == '':
+            events_to_do_time = '00:00:00'
+
+        extre_sth = input("please inpot extra sth:\n")
+
+        sql = """
+            insert into daily_events_arrangement 
+            (events_title,events_do_it_time,extre_sth)
+            values
+            ('%s','%s','%s')
+            """ % (title,events_to_do_time,extre_sth)
+            #print(sql)
+        self._direct_database_control(sql)
+        return state_machine[1]
--- main_code/first.py
+++ main_code/first.py
@@ -11,35 +11,56 @@ state_machine = {
     4:'delete',#
     5:'end',#
     6:'search',
+    7:'change_to_daily',
+    8:'change_to_normal',
+}
+
+table_state = {
+    1:"events_arrangement",
+    2:"daily_events_arrangement",
 }
 class Main_control_machine(object):
     # 初始化状态机、数据库模块
     def __init__ (self):
         self.database_control = database_mod.database_connect.Database_control()
+        self.daily_table_control = database_mod.database_connect.daily_table_control()
         self.state = state_machine[1]
     # 状态机启动
     def run(self):
+        temp_database = self.database_control
+        temp_table = table_state[1]
+
         while self.state != 'end':
             if self.state == 'spare':
-                self.database_control.print_data()
+                temp_database.print_data(temp_table)
                 try:
                     temp = int(input('input num:'))
                     self.state = state_machine[temp]
                 except:
-                    self.database_control.print_data()
+                    temp_database.print_data(temp_table)
             elif self.state == 'insert':
-                self.state = self.database_control.insert_into_database()
+                self.state = temp_database.insert_into_database()
             elif self.state == 'delete':
-                self.state=self.database_control.delete_id_row()
+                self.state=temp_database.delete_id_row()
             elif self.state == 'search':
                 pass
-                #self.state = self.database_control
+                #self.state = temp_database
             elif self.state == 'change':
-                self.state = self.database_control.change_database()
-            else:
-                self.database_control.end_connect()
-                sys.exit(0)            
-            self.database_control.end_connect()
+                self.state = temp_database.change_database()
+            elif self.state == 'change_to_daily':
+                self.state = state_machine[1]
+                temp_table = table_state[2]
+                temp_database = self.daily_table_control
+                continue
+            elif self.state == 'change_to_normal':
+                self.state = state_machine[1]
+                temp_table = table_state[1]
+                temp_database = self.database_control
+                continue
+            # else:
+            #     temp_database.end_connect()
+            #    % sys.exit(0)            
+            temp_database.end_connect()
 
 def start():
     main_control_machine = Main_control_machine()
