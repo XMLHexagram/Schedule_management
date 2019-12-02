fb80fbc32ac143d488b1337b6f0179eb71bb00da
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2019-12-02 21:09:18 +0800

    fine

--- hello_world/src/App.vue
+++ hello_world/src/App.vue
@@ -1,83 +1,99 @@
 <template>
-	<div id="app" style="">
-		<!-- <div id="app2"> -->
-	<InputForm></InputForm>
-		<!-- </div> -->
-		
-		<div v-for="affair in affairs" :key="affair.ID">
-			<div class="row at-row">
-				<div class="col-md-4">
-					<div class="at-box-row bg-c-brand-dark common">{{affair.ID}}</div>
-				</div>
-				<div class="col-md-4">
-					<div class="at-box-row bg-c-brand-light common">{{affair.events_title}}</div>
-				</div>
-				<div class="col-md-4">
-					<div class="at-box-row bg-c-brand-dark common">{{affair.events_deadline}}</div>
-				</div>
-				<div class="col-md-4">
-					<div class="at-box-row bg-c-brand-light common">{{affair.extre_sth}}</div>
-				</div>
-				<at-button v-on:click="deleteAffairs(affair.ID)" type="primary" style="float: left;">delete</at-button>
-				<at-button v-on:click="modifyAffairs(affair.ID)" type="primary" style="float: left;">modify</at-button>
-			</div>
-		</div>
-	</div>
+    <div id="app" style="">
+        <!-- <div id="app2"> -->
+        <InputForm></InputForm>
+        <!-- </div> -->
+        <div class="row at-row" style="position: relative;left: 145px">
+                <div class="col-md-4">
+                    <div class="at-box-row bg-c-brand-dark common">ID</div>
+                </div>
+                <div class="col-md-4">
+                    <div class="at-box-row bg-c-brand-light common">内容</div>
+                </div>
+                <div class="col-md-4">
+                    <div class="at-box-row bg-c-brand-dark common">创建时间</div>
+                </div>
+                <div class="col-md-4">
+                    <div class="at-box-row bg-c-brand-light common">附加内容</div>
+                </div>
+        </div>
+
+            <div v-for="affair in affairs" :key="affair.ID">
+                <div class="row at-row flex-center">
+                    <div class="col-md-4">
+                        <div class="at-box-row bg-c-brand-dark common">{{affair.ID}}</div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="at-box-row bg-c-brand-light common">{{affair.events_title}}</div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="at-box-row bg-c-brand-dark common">{{affair.events_deadline}}</div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="at-box-row bg-c-brand-light common">{{affair.extre_sth}}</div>
+                    </div>
+                    <at-button v-on:click="deleteAffairs(affair.ID)" type="primary" style="float: left;">delete
+                    </at-button>
+                    <at-button v-on:click="modifyAffairs(affair.ID)" type="primary" style="float: left;">modify
+                    </at-button>
+                </div>
+            </div>
+    </div>
 </template>
 
 <script>
-	import axios from 'axios';
-	import InputForm from "./components/InputForm";
+    import axios from 'axios';
+    import InputForm from "./components/InputForm";
 
-	export default {
-		name: 'app',
-		components: {InputForm},
-		data() {
-			return {
-				affairs: []
-			}
-		},
-		methods: {
-			deleteAffairs: function(ID) {
-				// alert(ID);
-				axios.delete('http://localhost:1221/opera/' + ID).then(() => {
-					this.getAllAffairs()
-				})
-				
-			},
-			modifyAffairs: function(ID) {
-				//回调
-				alert(ID);
-				this.getAllAffairs()
-			},
-			getAllAffairs: function() {
-				axios.get('http://localhost:1221/allAffairs').then(res => {
-					this.affairs = res.data.data
-					// console.log(this.affairs)
-				}).catch(err => {
-					this.$Message.error(err)
-				})
-			}
-		},
-		mounted() {
-			this.getAllAffairs();
-		},
-	}
+    export default {
+        name: 'app',
+        components: {InputForm},
+        data() {
+            return {
+                affairs: []
+            }
+        },
+        methods: {
+            deleteAffairs: function (ID) {
+                // alert(ID);
+                axios.delete('http://localhost:1221/opera/' + ID).then(() => {
+                    this.getAllAffairs()
+                })
+            },
+            modifyAffairs: function (ID) {
+                //回调
+                alert(ID);
+                InputForm.setDate(12,12,12,12,12,12,12,12);
+                this.getAllAffairs();
+            },
+            getAllAffairs: function () {
+                axios.get('http://localhost:1221/allAffairs').then(res => {
+                    this.affairs = res.data.data
+                    // console.log(this.affairs)
+                }).catch(err => {
+                    this.$Message.error(err)
+                })
+            }
+        },
+        mounted() {
+            this.getAllAffairs();
+        },
+    }
 </script>
 
 <style>
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-	div.common {
-		border-radius: 5px;
-		background-color: #66B3F3;
-		height: 30px;
-	}
+    div.common {
+        border-radius: 5px;
+        background-color: #66B3F3;
+        height: 30px;
+    }
 </style>
--- hello_world/src/components/InputForm.vue
+++ hello_world/src/components/InputForm.vue
@@ -1,12 +1,114 @@
 <template>
-    <div>
-        Hello
+    <div class="row at-row flex-center">
+        <!--        <input v-model="nowYear">-->
+        <!--        <p>{{nowYear}}</p>-->
+        <at-card style="width: 600px">
+            <h4>添加新事务</h4>
+            <div>
+                <!--                <p>{{this.nowTime}}</p>-->
+                <form>
+                    <at-input v-model="eventTitle" placeholder="events_title" style="width: 500px"></at-input>
+                    <at-input v-model="extraSth" placeholder="extra_sth" style="width: 500px"></at-input>
+                    <!--                    <at-input-number :min="1" :max="12"></at-input-number>-->
+                    <div class="row at-row">
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowYear" style="width: 35px"></at-input-number>
+                            <!--                            <span>{{nowYear}}</span>-->
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowMouth" style="width: 35px"></at-input-number>
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowDay" style="width: 35px"></at-input-number>
+                        </div>
+                    </div>
+                    <div class="row at-row">
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowHour" style="width: 35px"></at-input-number>
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowMinute" style="width: 35px"></at-input-number>
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <at-input-number v-model="nowSecond" style="width: 35px"></at-input-number>
+                            <!--                            <span>{{nowSecond}}</span>-->
+                        </div>
+                    </div>
+
+                    <at-button v-on:click="addAffair()" style="float: left"></at-button>
+                </form>
+            </div>
+        </at-card>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
+    let temp = new Date();
     export default {
-        name: "InputForm"
+        name: "InputForm",
+        data() {
+            let eventTitle;
+            let extraSth;
+            return {
+                // nowTime:{},
+                eventTitle,
+                extraSth,
+                nowYear: {},
+                nowMouth: {},
+                nowDay: {},
+                nowHour: {},
+                nowMinute: {},
+                nowSecond: {},
+            }
+        },
+        methods: {
+            getDate: function () {
+                // this.nowTime.year =temp.getFullYear();
+                // this.nowTime.mouth=temp.getMonth()+1;
+                // this.nowTime.day =temp.getDay();
+                // this.nowTime.hour = temp.getHours();
+                // this.nowTime.minute = temp.getMinutes();
+                // this.nowTime.second =temp.getSeconds();
+                // console.log(this.nowTime);
+                this.nowYear = temp.getFullYear();
+                this.nowMouth = temp.getMonth() + 1;
+                this.nowDay = temp.getDay();
+                this.nowHour = temp.getHours();
+                this.nowMinute = temp.getMinutes();
+                this.nowSecond = temp.getSeconds();
+            },
+            addAffair: function () {
+                axios({
+                    method: 'post',
+                    url: 'http://localhost:1221/opera/add',
+                    data: {
+                        events_title: this.eventTitle,
+                        events_deadline: this.nowYear + "/" + this.nowMouth + "/" + this.nowDay + " " + this.nowHour + "/" + this.nowMinute + "/" + this.nowSecond,
+                        extra_tips: this.extraSth
+                    }
+                });
+            },
+            setDate: function (year, month, day, hour, minute, second, title, sth) {
+                this.nowYear = year;
+                alert(year);
+                alert(this.nowYear);
+                this.nowMouth = month;
+                this.nowDay = day;
+                this.nowHour = hour;
+                this.nowMinute = minute;
+                this.nowSecond = second;
+                this.eventTitle = title;
+                this.extraSth = sth;
+            }
+        },
+        mounted() {
+            this.getDate()
+        },
+        created() {
+            this.getDate()
+        }
     }
 </script>
 
