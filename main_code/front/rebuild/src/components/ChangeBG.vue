30d9df2c42b5a2a97331191d3d7f5e45be373f84
legends-killer <958775729@qq.com>
2020-01-14 19:48:35 +0800

    add: change background func

---
+++ main_code/front/rebuild/src/components/ChangeBG.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <van-button type="info" v-on:click="sendDataToParent" style="margin: 10px" >更换背景</van-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'changeBG',
+    data(){
+        return{
+            childmsg: 1
+        }
+    },
+    methods:{
+        sendDataToParent: function () {
+            this.$emit('parentFunction', this.childmsg)
+            location.reload();
+      }
+    }
+  }
+</script>
