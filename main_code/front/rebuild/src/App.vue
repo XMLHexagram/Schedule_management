f4bcce6d75efb9a739c27f0d52ad076b52ba8d9c
legends-killer <958775729@qq.com>
2020-01-14 19:48:59 +0800

    enhance

--- main_code/front/rebuild/src/App.vue
+++ main_code/front/rebuild/src/App.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
+  <div class="outter" >
 <!--    <div id="nav">-->
 <!--      <router-link to="/">Home</router-link> |-->
 <!--      <router-link to="/about">About</router-link>-->
 <!--    </div>-->
+    <!-- <changeBG /> -->
     <router-view/>
   </div>
 </template>
 
 <style>
+  .outter {
+    margin-top: 0px;
+    width: 100%;
+    height: 100%;
+    padding: 0px;
+
+  }
 </style>
+
+<script>
+//   import changeBG from './components/ChangeBG'
+//   export default {
+//     data() {
+//       return {
+//         backGround: 'url(https://s0.xinger.ink/acgimg/acgurl.php)',
+//         ckey: 1,
+//       }
+//     },
+//     components:{
+//       changeBG,
+//     },
+//     methods: {
+//         customParentFunction(msg){
+//             console.log('子组件传过来的值',msg)
+//             this.ckey += 1
+//         }
+//     }
+//   }
+</script>
--- main_code/front/rebuild/src/views/Home.vue
+++ main_code/front/rebuild/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-        <van-tabs v-model="active" swipeable>
+    <div class="inner" :style="{ background: backGround }" :key="ckey" id="home">
+        <van-tabs v-model="active" swipeable style="
+                width: 100%;
+                height: 100%;
+                padding: 0px;">
             <van-tab title="每日任务">
                 <slot>
                     <DailyAffairs/>
                 </slot>
             </van-tab>
-            <van-tab title="长期事务">
+            <van-tab title="长期事务" style="
+                width: 100%;
+                height: 100%;
+                padding: 0px;">
                 <slot>
                     <Affairs/>
                 </slot>
             </van-tab>
+            <changeBG @parentFunction="ParentFunction"/>
         </van-tabs>
-
+        <!-- <child-component v-on:parentFunction = "customParentFunction"></child-component> -->
+        <!-- <child v-on:ParentFunction="ParentFunction"></child> -->
         <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     </div>
 </template>
@@ -21,20 +29,39 @@
     // @ is an alias to /src
     import DailyAffairs from '@/components/DailyAffairs.vue';
     import Affairs from "@/components/Affairs";
+    import changeBG from '../components/ChangeBG'
 
     export default {
         name: 'home',
+        props: ['parentFunction'],
         components: {
             DailyAffairs,
-            Affairs
+            Affairs,
+            changeBG,
         },
         data() {
             return {
-                active: 2
+                active: 2,
+                backGround: 'url(https://s0.xinger.ink/acgimg/acgurl.php)',
+                //https://s0.xinger.ink/fj/fjurl.php
+                //https://s0.xinger.ink/acgimg/acgurl.php
+                ckey: 1,
             };
+        },
+        methods: {
+            ParentFunction: function(msg){
+                console.log("fa")
+                console.log('子组件传过来的值',msg)
+                this.ckey += msg
+            }
         }
     }
 </script>
 
 <style scoped>
+    .inner {
+        width: 100%;
+        height: 100%;
+        padding: 0px;
+    }
 </style>
