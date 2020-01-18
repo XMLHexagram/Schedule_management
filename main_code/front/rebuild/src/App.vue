1b589a9e8144a3420534ac43239b1f0837437494
legends-killer <958775729@qq.com>
2020-01-18 16:39:39 +0800

    clean up

--- main_code/front/rebuild/src/App.vue
+++ main_code/front/rebuild/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="outter" >
-<!--    <div id="nav">-->
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--    </div>-->
-    <!-- <changeBG /> -->
     <router-view/>
   </div>
 </template>
@@ -20,22 +15,4 @@
 </style>
 
 <script>
-//   import changeBG from './components/ChangeBG'
-//   export default {
-//     data() {
-//       return {
-//         backGround: 'url(https://s0.xinger.ink/acgimg/acgurl.php)',
-//         ckey: 1,
-//       }
-//     },
-//     components:{
-//       changeBG,
-//     },
-//     methods: {
-//         customParentFunction(msg){
-//             console.log('子组件传过来的值',msg)
-//             this.ckey += 1
-//         }
-//     }
-//   }
 </script>
