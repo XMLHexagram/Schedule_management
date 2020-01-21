e18353638f10196e427cbddfaa8a44674d6e3b49
Hexagram <lmx_Hexagram@outlook.com>
2020-01-21 20:38:27 +0800

    修改获图片的接口,使其正确运行

--- main_code/back/src/imageSystem.go
+++ main_code/back/src/imageSystem.go
@@ -1,11 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"time"
+)
 
 
 func (s *Service) getImage(c *gin.Context) (int, interface{}) {
+	var sum int
+	s.DB.Table("image_urls").Count(&sum)
+	//fmt.Println(sum)
+
+	rand.Seed(time.Now().Unix())
+	tempID := rand.Intn(sum) + 1
+
+	//fmt.Println(tempID)
+
 	tempImageURL := new(imageURL)
-	s.DB.Take(tempImageURL)
+	s.DB.Table("image_urls").Where("id = ?",tempID).Find(tempImageURL)
 	return makeSuccessReturn(200,gin.H{"URL":tempImageURL.URL,"created_at":tempImageURL.CreatedAt})
 }
 
